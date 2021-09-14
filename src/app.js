0d5945854843c6b233bcc7ca04ebb8d7c9bc62dd
dipsam <codesalazar12@gmail.com>
2021-09-14 12:35:33 +0530

    input output processing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 var btnClick=document.querySelector("#btn-click");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-btnClick.addEventListener('click',()=>
-console.log('clicked'));
+
+
+/*can be made a callback function also*/
+btnClick.addEventListener('click',()=>{
+
+// console.log('clicked!');
+// console.log("input",txtInput.value);
+
+/*writing the value into output from textarea*/
+outputDiv.innerText="translate to banana "+txtInput.value;
+
+});
