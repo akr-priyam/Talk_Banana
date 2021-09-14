146e14705fcca3f92e7660451341c5486160fb03
dipsam <codesalazar12@gmail.com>
2021-09-14 16:01:56 +0530

    replaced the url

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var btnClick=document.querySelector("#btn-click");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
-var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl="https://api.funtranslations.com/translate/mandalorian.json";
 
 /*Function to return the url with additional parameters to fetch() function*/
 function callBackFunction(inputText){
@@ -15,7 +15,12 @@ btnClick.addEventListener('click',()=>{
     /*getting the input from textarea*/
     var inputText= txtInput.value;
     /*calling the url fetch, getting only translations part*/
-    fetch(callBackFunction(inputText)).then(response=>response.json()).then(json=>console.log(json.contents.translation))
+    fetch(callBackFunction(inputText)).then(response=>response.json()).then(json=>
+    {
+        /*getting the text from api and showing to the output*/
+        let translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
     .catch(error=>console.log(error));
 
 });
