5b812d4d6548f8219754ac5db52046b74a1c81cd
dipsam <codesalazar12@gmail.com>
2021-09-14 15:35:41 +0530

    wire fetch call(ex:9)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 var btnClick=document.querySelector("#btn-click");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
+var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+/*Function to return the url with additional parameters to fetch() function*/
+function callBackFunction(inputText){
+return serverUrl + "?" +"text=" +inputText;
+}
 
 
 /*can be made a callback function also*/
 btnClick.addEventListener('click',()=>{
 
-// console.log('clicked!');
-// console.log("input",txtInput.value);
-
-/*writing the value into output from textarea*/
-outputDiv.innerText="translate to banana "+txtInput.value;
+    /*getting the input from textarea*/
+    var inputText= txtInput.value;
+    /*calling the url fetch, getting only translations part*/
+    fetch(callBackFunction(inputText)).then(response=>response.json()).then(json=>console.log(json.contents.translation));
 
 });
